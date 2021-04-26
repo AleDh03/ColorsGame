f04427c18dc23f487ca56edeb91eb17361ec3b93
Alejo DH <alejo.hdez03@gmail.com>
2021-04-26 12:03:56 -0600

    first commit

---
+++ Js/index.js
@@ -0,0 +1,32 @@
+const celeste = document.getElementById('celeste');
+const violeta = document.getElementById('violeta');
+const naranja = document.getElementById('naranja');
+const verde =   document.getElementById('verde');
+const btnEmpezar = document.getElementById('btnEmpezar');
+
+class Juego {
+constructor() {
+    this.inicializar();
+    this.generarSecuencia();
+}
+
+inicializar() {
+        btnEmpezar.classList.add('hide')
+        this.nivel = 1;
+        this.colores  = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
+
+    }
+
+generarSecuencia() {
+        this.secuencia = new Array(10/*Cantidad de elementos que contiene el array*/ ).fill(0).map(n => Math.floor(Math.random() * 4));
+    }
+}
+
+function empezarJuego() {
+    window.juego = new Juego();
+}
