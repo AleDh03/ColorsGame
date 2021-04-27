4863ac6351c42bbde86be366ff2a662892ec7b72
Alejo DH <alejo.hdez03@gmail.com>
2021-04-27 14:55:45 -0600

    New changes

--- Js/index.js
+++ Js/index.js
@@ -1,32 +1,70 @@
-const celeste = document.getElementById('celeste');
-const violeta = document.getElementById('violeta');
-const naranja = document.getElementById('naranja');
-const verde =   document.getElementById('verde');
-const btnEmpezar = document.getElementById('btnEmpezar');
+const lightblue = document.getElementById('lightblue');
+const violet = document.getElementById('violet');
+const orange = document.getElementById('orange');
+const green =   document.getElementById('green');
+const btnStart = document.getElementById('btnStart');
 
-class Juego {
-constructor() {
-    this.inicializar();
-    this.generarSecuencia();
+//Constructor donde se sitúa toda la lógica
+class Game{
+constructor(){
+    this.initialize();
+    this.generateSequence();
+    this.nextLevel();
 }
 
-inicializar() {
-        btnEmpezar.classList.add('hide')
-        this.nivel = 1;
-        this.colores  = {
-            celeste,
-            violeta,
-            naranja,
-            verde
+//Primer método, esconde el "iniciar juego" al pulsar el botón
+initialize(){
+        btnStart.classList.add('hide')
+        this.level = 1;
+        this.colors  = {
+            lightblue,
+            violet,
+            orange,
+            green
         }
 
     }
 
-generarSecuencia() {
-        this.secuencia = new Array(10/*Cantidad de elementos que contiene el array*/ ).fill(0).map(n => Math.floor(Math.random() * 4));
-    }
+//Segundo método
+    generateSequence(){
+        this.sequence = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
 }
 
-function empezarJuego() {
-    window.juego = new Juego();
+//Tercer método
+    nextLevel(){
+        this.illuminateSequence()
+    }
+
+    transformFromNumberToColor(number){
+        switch (number) {
+            case 0:
+                return 'lightblue'
+            case 1:
+                return 'violet'
+            case 2:
+                return 'orange'
+            case 3:
+                return 'green'
+        }
+    }
+//La siguiente función pasa de numero a color por medio de un for
+    illuminateSequence()
+    {
+        for (let i = 0; i < this.level; i++) {
+        const color = this.transformFromNumberToColor(this.sequence[i]);
+        setTimeout(() => this.illuminateColor(color), 1000 * i);
+        }
+    }qqq
+    illuminateColor(color) {
+            this.colors[color].classList.add('light')
+            setTimeout(()=>this.turnOffColor(color), 350)
+        }
+
+    turnOffColor(color) {
+            this.colors[color].classList.remove('light')
+        }
+    }
+
+    function startGame() {
+        window.game = new Game();
 }
